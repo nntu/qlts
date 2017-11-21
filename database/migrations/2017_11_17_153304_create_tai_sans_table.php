0f90ed8a7a7e186e8b51129b98d049f32cecd202
ngoctuct <ngoctuct@gmail.com>
2017-11-21 09:40:58 +0700

    update table tài sản

--- database/migrations/2017_11_17_153304_create_tai_sans_table.php
+++ database/migrations/2017_11_17_153304_create_tai_sans_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateTaiSansTable extends Migration
 {
@@ -17,8 +17,7 @@ class CreateTaiSansTable extends Migration
             $table->string('MaTS')->primary();
             $table->string('TenTS');
             $table->string('MaLoaiTS');
-            $table->datetime('NgayDuaVaoSD');
-            $table->string('MaPhong');
+            $table->datetime('NgayDuaVaoSD');         
             $table->string('Serrial')->nullable();
             $table->timestamps();
         });
